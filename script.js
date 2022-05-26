2dd4b632f2882a0cc8ada98182d7975a934ffd09
kubwaSimon <kubwasimon@gmail.com>
2022-05-26 20:34:03 +0300

    Topstack

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+var Navlinks = document.getElementById("Navlinks");
+
+function showmenu() {
+    Navlinks.style.right = "0";
+}
+
+function hidemenu() {
+    Navlinks.style.right = "-200px";
+}
